a58627b4cde68780dd96b8b930b66f3ce47fbfce
Parthajit <parthajit@gmail.com>
2014-06-11 22:34:32 +0530

    Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+#makes cacheMatrix object, which has function for setting values to the matrix, getiing the vlaue, calculating inverse of the matrix
+#value of 'm' define the state of the matrix, if it is new or modified using set function then m=NULL
 
 makeCacheMatrix <- function(x = matrix()) {
+        #when a new cacheMatrix is created m would be set to NULL.
+        m <- NULL
+        set <- function(y) {
+                #assign input matrix to x, and m to null.
+                #Whenever the matrix changes m would be set to null
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 
 }
-
-
-## Write a short comment describing this function
-
+# cacheSolve takes a CacheMatrix, then it checks the 'm' value of the cacheMarix, if it is null it calculates the inverse, if not
+# it returns the already calculated value of inverse which was calculated during earlier call to cacheSolve function.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                #old matrix and matrix value not changed by set command.
+                # no need to recalculate just return m
+                message("getting cached data")
+                return(m)
+        }
+        # will reach here only if matrix is new or changed.
+        # get matrix
+        data <- x$get()
+        #calculate the inverse
+        m <- solve(data, ...)
+        # store this inverse for future use.
+        x$setsolve(m)
+        #return the inverse.
+        m
 }
